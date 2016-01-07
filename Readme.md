f8383a31aa04ab14b1589f43b203c8bb6612d6d4
Jack Parkhouse <jparkhouse20@jparkhouse20.local>
2016-01-07 14:33:57 -0700

    add Readme

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# first day Repo
+
+this is the code from the first day of MS Advanced topics
---
+++ main.py
@@ -0,0 +1,7 @@
+import ahoff
+import cmalley
+import gmoltz
+import jgreco
+import jkoeplin
+import jparkhouse
+import vramsey
